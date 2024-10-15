fd8eb6a2e7451174f8ef8ceb408b8b074abe4d2c
savinda <thantrige32@gmail.com>
2024-10-15 16:51:37 +0530

    validate contact form

--- src/app/components/ContactUs.js
+++ src/app/components/ContactUs.js
@@ -1,12 +1,53 @@
-'use client';
+"use client";
 import { Link } from "react-scroll";
 import Navbar from "./Navbar";
 import { FiHexagon, FiMail, FiMapPin, FiPhone } from "react-icons/fi";
 import Footer from "./Footer";
 import UseScroll from "../hooks/UseScroll";
+import { useState } from "react";
 
 export default function ContactUs() {
+  const [email, setEmail] = useState({
+    email: "",
+    subject: "",
+    messege: "",
+    name: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    let formErrors = {};
+
+    if (!email.name.trim()) {
+      formErrors.name = "Name is required";
+    }
+
+    if (!email.email) {
+      formErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(email.email)) {
+      formErrors.email = "Email address is invalid";
+    }
+
+    if (!email.messege.trim()) {
+      formErrors.message = "Message is required";
+    }
+
+    return formErrors;
+  };
+
+  const handleEmailSend = (e) => {
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+      console.log("Form Submitted", email);
+    }
+  };
+
   const isScrolled = UseScroll();
+
   return (
     <>
       <Navbar
@@ -45,9 +86,9 @@ export default function ContactUs() {
             </nav>
           </div>
         </div>
-        </section>
+      </section>
 
-        <div className="position-relative">
+      <div className="position-relative">
         <div className="shape overflow-hidden text-white">
           <svg
             viewBox="0 0 2880 48"
@@ -62,187 +103,213 @@ export default function ContactUs() {
         </div>
       </div>
 
-        <section className="section pb-0">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <div className="p-4 rounded-3 shadow">
-                  <form>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="mb-3">
-                          <label className="form-label">
-                            Your Name <span className="text-danger">*</span>
-                          </label>
-                          <input
-                            name="name"
-                            id="name"
-                            type="text"
-                            className="form-control"
-                            placeholder="Name :"
-                          />
-                        </div>
-                      </div>
-
-                      <div className="col-md-6">
-                        <div className="mb-3">
-                          <label className="form-label">
-                            Your Email <span className="text-danger">*</span>
-                          </label>
-                          <input
-                            name="email"
-                            id="email"
-                            type="email"
-                            className="form-control"
-                            placeholder="Email :"
-                          />
-                        </div>
-                      </div>
-
-                      <div className="col-12">
-                        <div className="mb-3">
-                          <label className="form-label">Subject</label>
-                          <input
-                            name="subject"
-                            id="subject"
-                            className="form-control"
-                            placeholder="Subject :"
-                          />
-                        </div>
-                      </div>
-
-                      <div className="col-12">
-                        <div className="mb-3">
-                          <label className="form-label">
-                            Comments <span className="text-danger">*</span>
-                          </label>
-                          <textarea
-                            name="comments"
-                            id="comments"
-                            rows="4"
-                            className="form-control"
-                            placeholder="Message :"
-                          ></textarea>
-                        </div>
+      <section className="section pb-0">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="p-4 rounded-3 shadow">
+                <form>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label className="form-label">
+                          Your Name <span className="text-danger">*</span>
+                        </label>
+                        <input
+                          onChange={(e) =>
+                            setEmail({ ...email, name: e.target.value })
+                          }
+                          name="name"
+                          id="name"
+                          type="text"
+                          className="form-control"
+                          placeholder="Name :"
+                        />
+                        {errors.name && (
+                          <small className="text-danger">{errors.name}</small>
+                        )}
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-12">
-                        <div className="d-grid">
-                          <button
-                            type="submit"
-                            id="submit"
-                            name="send"
-                            className="btn btn-primary"
-                          >
-                            Send Message
-                          </button>
-                        </div>
+
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label className="form-label">
+                          Your Email <span className="text-danger">*</span>
+                        </label>
+                        <input
+                          onChange={(e) =>
+                            setEmail({ ...email, email: e.target.value })
+                          }
+                          name="email"
+                          id="email"
+                          type="email"
+                          className="form-control"
+                          placeholder="Email :"
+                        />
+                        {errors.email && (
+                          <small className="text-danger">{errors.email}</small>
+                        )}
                       </div>
                     </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <div className="container mt-100 mt-60">
-            <div className="row g-4">
-              <div className="col-md-4">
-                <div className="position-relative features text-center mx-lg-4 px-md-1">
-                  <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
-                    <FiHexagon className="hexagon" />
-                    <div className="position-absolute top-50 start-50 translate-middle">
-                      <FiPhone className="fea icon-m-md text-primary" />
+                    <div className="col-12">
+                      <div className="mb-3">
+                        <label className="form-label">Subject</label>
+                        <input
+                          onChange={(e) =>
+                            setEmail({ ...email, subject: e.target.value })
+                          }
+                          name="subject"
+                          id="subject"
+                          className="form-control"
+                          placeholder="Subject :"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-12">
+                      <div className="mb-3">
+                        <label className="form-label">
+                          Comments <span className="text-danger">*</span>
+                        </label>
+                        <textarea
+                          onChange={(e) =>
+                            setEmail({ ...email, message: e.target.value })
+                          }
+                          name="comments"
+                          id="comments"
+                          rows="4"
+                          className="form-control"
+                          placeholder="Message :"
+                        ></textarea>
+                        {errors.message && (
+                          <small className="text-danger">
+                            {errors.message}
+                          </small>
+                        )}
+                      </div>
                     </div>
                   </div>
 
-                  <div className="mt-4">
-                    <h5 className="mb-3">Phone</h5>
-                    <p className="text-muted">
-                      Start working with Towntor that can provide everything
-                    </p>
-                    <Link href="tel:+61 0455 474 220" className="text-primary">
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="d-grid">
+                        <button
+                          onClick={handleEmailSend}
+                          type="button"
+                          id="submit"
+                          name="send"
+                          className="btn btn-primary"
+                        >
+                          Send Message
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container mt-100 mt-60">
+          <div className="row g-4">
+            <div className="col-md-4">
+              <div className="position-relative features text-center mx-lg-4 px-md-1">
+                <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
+                  <FiHexagon className="hexagon" />
+                  <div className="position-absolute top-50 start-50 translate-middle">
+                    <FiPhone className="fea icon-m-md text-primary" />
+                  </div>
+                </div>
+
+                <div className="mt-4">
+                  <h5 className="mb-3">Phone</h5>
+                  <p className="text-muted">
+                    Start working with Towntor that can provide everything
+                  </p>
+                  <Link href="tel:+61 0455 474 220" className="text-primary">
                     (+61) 0455 474 220
-                    </Link>
-                  </div>
+                  </Link>
                 </div>
               </div>
+            </div>
 
-              <div className="col-md-4">
-                <div className="position-relative features text-center mx-lg-4 px-md-1">
-                  <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
-                    <FiHexagon className="hexagon" />
-                    <div className="position-absolute top-50 start-50 translate-middle">
-                      <FiMail className="fea icon-m-md text-primary" />
-                    </div>
-                  </div>
-
-                  <div className="mt-4">
-                    <h5 className="mb-3">Email</h5>
-                    <p className="text-muted">
-                      Start working with Towntor that can provide everything
-                    </p>
-                    <Link
-                      href="mailto:randy.navaratne@the5thavenue.com.au"
-                      className="text-primary"
-                    >
-                      randy.navaratne@the5thavenue.com.au
-                    </Link>
+            <div className="col-md-4">
+              <div className="position-relative features text-center mx-lg-4 px-md-1">
+                <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
+                  <FiHexagon className="hexagon" />
+                  <div className="position-absolute top-50 start-50 translate-middle">
+                    <FiMail className="fea icon-m-md text-primary" />
                   </div>
                 </div>
+
+                <div className="mt-4">
+                  <h5 className="mb-3">Email</h5>
+                  <p className="text-muted">
+                    Start working with Towntor that can provide everything
+                  </p>
+                  <Link
+                    href="mailto:randy.navaratne@the5thavenue.com.au"
+                    className="text-primary"
+                  >
+                    randy.navaratne@the5thavenue.com.au
+                  </Link>
+                </div>
               </div>
+            </div>
 
-              <div className="col-md-4">
-                <div className="position-relative features text-center mx-lg-4 px-md-1">
-                  <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
-                    <FiHexagon className="hexagon" />
-                    <div className="position-absolute top-50 start-50 translate-middle">
-                      <FiMapPin className="fea icon-m-md text-primary" />
-                    </div>
+            <div className="col-md-4">
+              <div className="position-relative features text-center mx-lg-4 px-md-1">
+                <div className="feature-icon position-relative overflow-hidden d-flex justify-content-center">
+                  <FiHexagon className="hexagon" />
+                  <div className="position-absolute top-50 start-50 translate-middle">
+                    <FiMapPin className="fea icon-m-md text-primary" />
                   </div>
+                </div>
 
-                  <div className="mt-4">
-                    <h5 className="mb-3">Location</h5>
-                    <p className="text-muted">
-                    The 5th Avenue Real Estate Waterman Business Centre, Chadstone VIC 3168
-                    </p>
-                    <Link
-                      href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
-                      className="lightbox text-primary"
-                      data-type="iframe"
-                      data-group="iframe"
-                      data-width="1024px"
-                      data-height="576px"
-                    >
-                      View on Google map
-                    </Link>
-                  </div>
+                <div className="mt-4">
+                  <h5 className="mb-3">Location</h5>
+                  <p className="text-muted">
+                    The 5th Avenue Real Estate Waterman Business Centre,
+                    Chadstone VIC 3168
+                  </p>
+                  <Link
+                    href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+                    className="lightbox text-primary"
+                    data-type="iframe"
+                    data-group="iframe"
+                    data-width="1024px"
+                    data-height="576px"
+                  >
+                    View on Google map
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="container mt-100 mt-60">
-            <div className="row justify-content-center">
-              <div className="col-12 col-lg-8 p-0">
-                <div className="card map border-0">
-                  <div className="card-body p-0">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
-                      style={{ border: "0" }}
-                      title="5th Avenue"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
+        <div className="container mt-100 mt-60">
+          <div className="row justify-content-center">
+            <div className="col-12 col-lg-8 p-0">
+              <div className="card map border-0">
+                <div className="card-body p-0">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+                    style={{ border: "0" }}
+                    title="5th Avenue"
+                    allowFullScreen
+                  ></iframe>
                 </div>
               </div>
             </div>
           </div>
-          <div className="mt-100">
-            <Footer />
-          </div>
-        </section>
+        </div>
+        <div className="mt-100">
+          <Footer />
+        </div>
+      </section>
     </>
   );
 }
