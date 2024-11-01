7ea0893c045f95bb2e6fd17af735195bf95d306f
savindaJ <thantrige32@gmail.com>
2024-11-01 16:56:34 +0530

    add componenets

--- src/app/aboutus/viewMember/page.js
+++ src/app/aboutus/viewMember/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/app/components/navbar";
 import { teamData } from "@/app/data/data";
 import Image from "next/image";
 import Link from "next/link";
