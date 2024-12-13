3e3a3ddfcfa5915d8bd8ce5100ccea809e6a361f
savinda <thantrige32@gmail.com>
2024-12-13 09:14:41 +0530

    chore: remove commented-out sections in Subscribe and WhoWeAre components to clean up code

--- src/app/components/subscribe.js
+++ src/app/components/subscribe.js
@@ -10,59 +10,6 @@ export const Subscribe = () => {
   return (
     <>
       <div className="row justify-content-center mt-100" data-aos="fade-right">
-        {/* <section
-          className="position-relative z-1"
-          style={{
-            backgroundColor: "#000",
-            zIndex: 1,
-          }}
-        >
-          <div className="w-100 row h-100 d-flex text-center justify-content-center align-items-center position-absolute z-2">
-            <div>
-              <p className="text-white font-monospace text-center txt-subscribe">
-                Subscribe to be stay <br />
-                updated with us
-              </p>
-              <div className="row justify-content-center align-items-center">
-                <button
-                  onClick={() => {
-                    router.push("/contactus");
-                  }}
-                  className="btn btn-primary w-25 w-md-25"
-                  type="button"
-                >
-                  Contact Us
-                </button>
-              </div>
-            </div>
-          </div>
-          <div
-            className="text-center"
-            style={{
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-              zIndex: 1,
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-            }}
-          ></div>
-          <Image
-            src="/images/bg/02.jpg"
-            width={10}
-            height={0}
-            sizes="50vw"
-            style={{
-              width: "100%",
-              height: "80vh",
-              zIndex: 0,
-              objectFit: "cover",
-            }}
-            className="img-fluid rounded-3"
-            alt="%th avenue"
-            title="Toavenuewnter"
-          />
-        </section> */}
-
         <section className="subcription-bg">
           <div className="bg-container">
             <div className="bg-overlay"></div>
--- src/app/components/whoWeAre.js
+++ src/app/components/whoWeAre.js
@@ -18,62 +18,6 @@ export const WhoWeAre = () => {
           </div>
         </div>
 
-        {/* <section
-          className="position-relative z-1"
-          style={{
-            backgroundColor: "#000",
-            zIndex: 1,
-          }}
-        >
-          <div className="w-100 h-100 d-flex text-center justify-content-center align-items-center position-absolute z-2">
-            <div>
-              <p className="text-white font-monospace text-center who-we-are-main-text">
-                Chef Wow - the "Teen Vibe" Restaurant Project
-              </p>
-              <p className="text-white text-center w-75 w-md-50 mx-auto who-we-are-p">
-                We are a team of professionals dedicated to providing the best
-                possible service to our clients. With wide experience in the
-                real estate industry, we are committed to excellence.
-              </p>
-
-              <div className="row">
-                <button
-                onClick={()=>{
-                  router.push("/aboutus")
-                }}
-                className="btn btn-primary m-auto w-25 btn-contact">
-                  About Us
-                </button>
-              </div>
-            </div>
-          </div>
-          <div
-            className="text-center"
-            style={{
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-              zIndex: 1,
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-            }}
-          ></div>
-          <Image
-            src="/images/who-we-are.jpg"
-            width={10}
-            height={0}
-            sizes="50vw"
-            style={{
-              width: "100%",
-              height: "80vh",
-              zIndex: 0,
-              objectFit: "cover",
-            }}
-            className="img-fluid rounded-3"
-            alt="%th avenue"
-            title="Toavenuewnter"
-          />
-        </section> */}
-
         <section className="container">
           <div className="row">
             <div className="col-lg-6 col-md-6 col-12 p-3 who-weare-bg"></div>
